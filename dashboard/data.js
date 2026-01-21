fba7cb3791f46680f62416275f24ebb85f22be54
Krish Shah <kshah@trueigtech.com>
2026-01-21 12:08:47 +0530

    created: Handles dashboard data

--- dashboard/data.js
+++ dashboard/data.js
@@ -5,6 +5,15 @@ export const displayData = () => {
     const completed = tasks.filter(t => t.status === 'completed');
     const ongoing = tasks.filter(t => t.status === 'ongoing');
 
+    const totalTimeSpent = completed.reduce((sum, task) => sum + (task.timeSpent || 0), 0);
+
+    const avgCompletionTime = completed.length > 0 
+        ? (totalTimeSpent / completed.length).toFixed(2) 
+        : 0;
+
+    const percentage = tasks.length > 0 ? ((completed.length / tasks.length) * 100).toFixed(0) : 0;
+
+    document.getElementById('dashboard-time-spent').innerHTML = `<p><b>Avg. Completion Time:</b> ${avgCompletionTime} seconds</p>`;
     document.getElementById('dashboard-total-tasks').innerHTML = `<b>Total Tasks: ${tasks.length} </b>`;
     document.getElementById('dashboard-completed-tasks').innerHTML = `<b>Completed Tasks: ${completed.length} </b>`
     document.getElementById('dashboard-ongoing-tasks').innerHTML = `<b> Ongoing Tasks: ${ongoing.length} </b>`
